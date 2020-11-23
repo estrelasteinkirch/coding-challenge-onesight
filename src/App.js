6e0eb68cd113ae48062650b583598792cb7e1ff0
Estrela <estrela.steinkirch@gmail.com>
2020-11-24 00:12:37 +0100

    Install and initialize Material-UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Typography from '@material-ui/core/Typography';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -6,9 +7,17 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route path="/receitas/:id"><h2>Receita Número 1</h2></Route>
-          <Route path="/receitas"><h2>Lista de Receitas</h2></Route>
-          <Route path="/"><h2>Login</h2></Route>
+          <Route path="/receitas/:id">
+            <h2>Receita Número 1</h2>
+          </Route>
+          <Route path="/receitas">
+            <h2>Lista de Receitas</h2>
+          </Route>
+          <Route path="/">
+            <Typography variant="h1" gutterBottom>
+              Login
+            </Typography>
+          </Route>
         </Switch>
       </div>
     </Router>
