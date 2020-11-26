83d729ce80be0aff9cb2aaa3ee4f0e928a52c1cc
Estrela <estrela.steinkirch@gmail.com>
2020-11-27 00:37:04 +0100

    Add Component Footer

--- src/App.js
+++ src/App.js
@@ -5,29 +5,42 @@ import LoginPage from "./features/login/LoginPage";
 import RecipeDetailsPage from "./features/recipeDetails/RecipeDetailsPage";
 import RecipesPage from "./features/recipeList/RecipesPage";
 import Navbar from "./components/Navbar";
-import { Container } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Switch>
-          <PrivateRoute path="/recipes/:id">
-            <RecipeDetailsPage />
-          </PrivateRoute>
-          <PrivateRoute path="/recipes">
-            <RecipesPage />
-          </PrivateRoute>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </Container>
+      <div className={classes.root}>
+        <Navbar />
+        <Container>
+          <Switch>
+            <PrivateRoute path="/recipes/:id">
+              <RecipeDetailsPage />
+            </PrivateRoute>
+            <PrivateRoute path="/recipes">
+              <RecipesPage />
+            </PrivateRoute>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </div>
     </Router>
   );
 }
