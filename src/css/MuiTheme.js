9df8d83dd06e860c0ef61f2265cd0e2e0b3593f4
Estrela <estrela.steinkirch@gmail.com>
2020-11-26 22:58:50 +0100

    Named the project. Add Material UI at Recipe Details Page

--- src/css/MuiTheme.js
+++ src/css/MuiTheme.js
@@ -1,18 +1,33 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#ffd0b0',
-      main: '#ff9e80',
-      dark: '#c96f53',
-      contrastText: '#000000',
+      light: "#ffd0b0",
+      main: "#ff9e80",
+      dark: "#c96f53",
+      contrastText: "#000000",
     },
     secondary: {
-      light: '#ff6434',
-      main: '#dd2c00',
-      dark: '#a30000',
-      contrastText: '#ffffff',
+      light: "#ff6434",
+      main: "#c96f53",
+      dark: "#a30000",
+      contrastText: "#ffffff",
+    },
+    youtube: {
+      main: "#dd2c00",
+    },
+  },
+  typography: {
+    h4: {
+      fontWeight: 600,
+    },
+    h3: {
+      fontWeight: 600,
+    },
+    h6: {
+      fontWeight: 600,
+      fontStyle: "italic",
     },
   },
 });
